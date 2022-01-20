d909bdef4e296a858c448f761eb7e96de84a0e60
Nuno Facha <nunofacha@gmail.com>
2022-01-20 13:09:36 -0100

    Update gitignore

--- build/enum/TrackSource.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TrackSource = void 0;
-var TrackSource;
-(function (TrackSource) {
-    TrackSource["OPEN_SKY_NETWORK"] = "OPSN";
-})(TrackSource = exports.TrackSource || (exports.TrackSource = {}));
