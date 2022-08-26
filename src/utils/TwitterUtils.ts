539b4bf3742cdd045fd8d77f204722d4f836eb24
Nuno Facha <nunofacha@gmail.com>
2022-08-26 17:32:05 +0000

    Banana

--- src/utils/TwitterUtils.ts
+++ src/utils/TwitterUtils.ts
@@ -3,6 +3,9 @@ import {PlaneAlert} from "../index";
 
 export default class TwitterUtils {
     public static getTwitterClient(token:string, secret:string){
+        if (PlaneAlert.config.twitter.appToken === null || PlaneAlert.config.twitter.appSecret === null) {
+            return null;
+        }
         return new TwitterApi(
             {
                 appKey: PlaneAlert.config.twitter.appToken,
