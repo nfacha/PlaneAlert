28a409632dfc97e15a97e9502872ffb871934414
Nuno Facha <nunofacha@gmail.com>
2022-08-27 13:36:09 +0000

    Add PlaneUtils

---
+++ src/utils/PlaneUtils.ts
@@ -0,0 +1,19 @@
+export class PlaneUtils {
+    public static isEmergencySquawk(squawk: string): boolean {
+        return ['7500', '7600', '7700'].includes(squawk)
+    }
+
+    public static getEmergencyType(squawk: string): string {
+        switch (squawk) {
+            case '7500':
+                return 'Hijack';
+            case '7600':
+                return 'Radio Failure';
+            case '7700':
+                return 'General Emergency';
+            default:
+                return '';
+
+        }
+    }
+}
