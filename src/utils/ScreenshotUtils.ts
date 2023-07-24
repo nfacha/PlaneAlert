4fc471edb7a64e1eed15bbf79f5cf71a9cb23a16
Nuno Facha <nunofacha@gmail.com>
2023-07-24 09:49:15 +0000

    Update ScreenshotUtils.ts to set headless value to "new".

--- src/utils/ScreenshotUtils.ts
+++ src/utils/ScreenshotUtils.ts
@@ -13,7 +13,7 @@ export class ScreenshotUtils {
                         width: 1920,
                         height: 1080,
                     },
-                    headless: true,
+                    headless: "new"
                 })
                 .then(async (browser: Browser) => {
                     const page = await browser.newPage();
@@ -43,7 +43,7 @@ export class ScreenshotUtils {
                         width: 1920,
                         height: 1080,
                     },
-                    headless: true,
+                    headless: "new"
                 })
                 .then(async (browser: Browser) => {
                     const page = await browser.newPage();
